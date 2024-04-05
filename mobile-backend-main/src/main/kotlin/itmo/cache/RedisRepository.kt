8ae35336a0c9011868b0f2df6578a73a0e09d384
Artem <tuchin12345678artem@gmail.com>
2024-04-05 12:46:46 +0300

    redis cache interface update methods

--- mobile-backend-main/src/main/kotlin/itmo/cache/RedisRepository.kt
+++ mobile-backend-main/src/main/kotlin/itmo/cache/RedisRepository.kt
@@ -3,21 +3,13 @@ package itmo.cache
 import redis.clients.jedis.Jedis
 import redis.clients.jedis.JedisPool
 
-interface RedisRepository<T> {
+interface RedisRepository<T, V> {
     val jedis : Jedis
         get() = JedisPool().resource
 
     suspend fun addItem(name: String, item : T, time : Long = 60000)
 
-    suspend fun getItem(name: String) : Map<String, String> {
-        return jedis.hgetAll(name)
-    }
+    suspend fun getItem(name: String) : V
 
-    suspend fun getField(name: String, field: String) : String? {
-        return getItem(name)[field]
-    }
-
-    suspend fun isItemExists(name: String) : Boolean {
-        return jedis.exists(name)
-    }
+    suspend fun isItemExists(name: String) : Boolean
 }
