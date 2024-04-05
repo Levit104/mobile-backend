238060baad47859f04494e36a7d029622f79d33e
Artem <tuchin12345678artem@gmail.com>
2024-04-05 13:21:50 +0300

    deviceType redis repository

---
+++ mobile-backend-main/src/main/kotlin/itmo/cache/model/DeviceType.kt
@@ -0,0 +1,23 @@
+package itmo.cache.model
+
+import itmo.cache.RedisRepository
+
+class DeviceTypeRedisRepository : RedisRepository<String, String> {
+    override suspend fun addItem(typeId: String, typeName: String, time: Long) {
+        jedis.set("device_type#$typeId", typeName)
+        jedis.sadd("device_types", typeId)
+    }
+
+    override suspend fun getItem(typeId: String): String {
+        return jedis.get("device_type#$typeId")
+    }
+
+    override suspend fun isItemExists(typeId: String): Boolean {
+        return jedis.exists("device_type#$typeId")
+    }
+
+    suspend fun getItems() : Map<String, String> {
+        val set = jedis.smembers("device_types")
+        return set.associateWith { s -> jedis.get("device_type#$s") }
+    }
+}
