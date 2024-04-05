1208615a8bd798fc4e0251ad7a784107198e1df8
Artem <tuchin12345678artem@gmail.com>
2024-04-05 13:22:04 +0300

    user redis repository

--- mobile-backend-main/src/main/kotlin/itmo/cache/model/User.kt
+++ mobile-backend-main/src/main/kotlin/itmo/cache/model/User.kt
@@ -1,16 +1,34 @@
 package itmo.cache.model
 
 import itmo.cache.RedisRepository
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class UserDAO (
-    val login: String,
+    var id : Long?,
+    val username: String,
     val password: String
 )
 
-class UserRedisRepository : RedisRepository<UserDAO> {
-    override suspend fun addItem(name: String, item: UserDAO, time: Long) {
-        jedis.hset(name, "login", item.login)
-        jedis.hset(name, "password", item.password)
-        jedis.pexpire(name, time)
+class UserRedisRepository : RedisRepository<UserDAO, Map<String, String>> {
+
+    override suspend fun getItem(userId: String): Map<String, String> {
+        return jedis.hgetAll("user#$userId")
+    }
+
+    override suspend fun isItemExists(username: String): Boolean {
+        return jedis.exists("username#$username")
+    }
+
+    override suspend fun addItem(userId: String, item: UserDAO, time: Long) {
+        jedis.hset("user#$userId", "id", item.id.toString())
+        jedis.hset("user#$userId", "login", item.username)
+        jedis.hset("user#$userId", "password", item.password)
+        jedis.pexpire("user#$userId", time)
+        jedis.setex("username#${item.username}", time, userId)
+    }
+
+    suspend fun getUserByLogin(login: String) : Map<String, String>{
+        return getItem(jedis.get("username#$login"))
     }
 }
