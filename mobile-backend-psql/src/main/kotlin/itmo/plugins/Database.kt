60a6e6f7dc002520dd6e67dbcbae1fae590c76c9
Artyom <artyom0104@gmail.com>
2024-04-25 01:42:56 +0300

    Add HikariCP

--- mobile-backend-psql/src/main/kotlin/itmo/plugins/Database.kt
+++ mobile-backend-psql/src/main/kotlin/itmo/plugins/Database.kt
@@ -1,5 +1,6 @@
 package itmo.plugins
 
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import itmo.models.*
 import org.jetbrains.exposed.sql.Database
@@ -7,12 +8,25 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabase() {
+    /*
     val database = Database.connect(
         url = "jdbc:postgresql://localhost:5432/mobile",
         driver = "org.postgresql.Driver",
         user = "postgres",
-        password = "1112"
+        password = "qwerty123"
     )
+     */
+
+    val dataSource = HikariDataSource().apply {
+        jdbcUrl = "jdbc:postgresql://localhost:5432/mobile"
+        driverClassName = "org.postgresql.Driver"
+        username = "postgres"
+        password = "qwerty123"
+        maximumPoolSize = 8
+    }
+
+    val database = Database.connect(dataSource)
+
     transaction(database) {
         SchemaUtils.create(Users)
         SchemaUtils.create(Rooms)
