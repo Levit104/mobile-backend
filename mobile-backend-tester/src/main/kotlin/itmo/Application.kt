9b92304c2f79cd9e58d910b14aebb7ecab9cf491
Artem <tuchin12345678artem@gmail.com>
2024-05-23 20:30:31 +0300

    Jedis pool

--- mobile-backend-tester/src/main/kotlin/itmo/Application.kt
+++ mobile-backend-tester/src/main/kotlin/itmo/Application.kt
@@ -7,21 +7,39 @@ import io.ktor.serialization.kotlinx.json.*
 import itmo.plugins.startTest
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import redis.clients.jedis.JedisPool
+import redis.clients.jedis.JedisPoolConfig
+
 
 suspend fun main() {
 //    val engine = embeddedServer(Netty, port = 8085, host = "0.0.0.0", module = Application::module)
 //        .start(wait = true)
     startTest()
     client.close()
+    jedisPool.destroy()
 }
 
-//val logger = LoggerFactory.getLogger("my")
+val poolConfig = buildPoolConfig()
+var jedisPool = JedisPool(poolConfig)
+
+private fun buildPoolConfig(): JedisPoolConfig {
+    val poolConfig = JedisPoolConfig()
+    poolConfig.maxTotal = 128
+    poolConfig.maxIdle = 128
+    poolConfig.minIdle = 16
+    poolConfig.testOnBorrow = true
+    poolConfig.testOnReturn = true
+    poolConfig.testWhileIdle = true
+    poolConfig.numTestsPerEvictionRun = 3
+    poolConfig.blockWhenExhausted = true
+    return poolConfig
+}
 
 @OptIn(ExperimentalSerializationApi::class)
 val client = HttpClient(CIO) {
     engine {
-        endpoint.maxConnectionsPerRoute = 250
-        maxConnectionsCount = 10000
+        endpoint.maxConnectionsPerRoute = 100
+        maxConnectionsCount = 1000
         endpoint.connectAttempts = 5
         endpoint.keepAliveTime = 1
     }
--- mobile-backend-tester/src/main/kotlin/itmo/util/JedisManager.kt
+++ mobile-backend-tester/src/main/kotlin/itmo/util/JedisManager.kt
@@ -1,10 +1,11 @@
 package itmo.util
 
+import itmo.jedisPool
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import redis.clients.jedis.JedisPool
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 fun log(event: String, userId: String, description: String, status: String) {
     val message = MessageLogDao(
@@ -12,5 +13,5 @@ fun log(event: String, userId: String, description: String, status: String) {
             Date()
         ), description, status
     )
-    JedisPool().resource.publish("LoggerQueue", Json.encodeToString(message))
+    jedisPool.resource.publish("LoggerQueueTester", Json.encodeToString(message))
 }
