e3a16ea21265b46c7d0b96256a3ffc7cc98a1ee5
Artem <tuchin12345678artem@gmail.com>
2024-05-23 20:33:13 +0300

    jedis pool

--- mobile-backend-logger/src/main/kotlin/itmo/Application.kt
+++ mobile-backend-logger/src/main/kotlin/itmo/Application.kt
@@ -7,6 +7,8 @@ import itmo.db.LogDataBase
 import itmo.db.schedulerInit
 import itmo.plugins.*
 import kotlinx.coroutines.launch
+import redis.clients.jedis.JedisPool
+import redis.clients.jedis.JedisPoolConfig
 
 lateinit var logDB: LogDataBase
 fun main() {
@@ -17,6 +19,22 @@ fun main() {
 
     embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
+}
+
+val poolConfig = buildPoolConfig()
+var jedisPool = JedisPool(poolConfig)
+
+private fun buildPoolConfig(): JedisPoolConfig {
+    val poolConfig = JedisPoolConfig()
+    poolConfig.maxTotal = 128
+    poolConfig.maxIdle = 128
+    poolConfig.minIdle = 16
+    poolConfig.testOnBorrow = true
+    poolConfig.testOnReturn = true
+    poolConfig.testWhileIdle = true
+    poolConfig.numTestsPerEvictionRun = 3
+    poolConfig.blockWhenExhausted = true
+    return poolConfig
 }
 
 fun Application.module() {
--- mobile-backend-logger/src/main/kotlin/itmo/JedisManager.kt
+++ mobile-backend-logger/src/main/kotlin/itmo/JedisManager.kt
@@ -2,17 +2,36 @@ package itmo
 
 import itmo.db.LogDataBase
 import itmo.model.MessageLogDao
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
-import redis.clients.jedis.Jedis
-import redis.clients.jedis.JedisPool
 import redis.clients.jedis.JedisPubSub
 
 suspend fun psqlLoggerInit(logDb: LogDataBase) {
-    val jedis : Jedis = JedisPool().resource
-    jedis.subscribe(object : JedisPubSub() {
-        override fun onMessage(channel : String, message : String) {
-            println(Json.decodeFromString<MessageLogDao>(message))
-            logDb.insertLog(Json.decodeFromString<MessageLogDao>(message))
+    coroutineScope {
+        launch {
+            jedisPool.resource.subscribe(object : JedisPubSub() {
+                override fun onMessage(channel: String, message: String) {
+                    println(Json.decodeFromString<MessageLogDao>(message))
+                    logDb.insertLog(Json.decodeFromString<MessageLogDao>(message))
+                }
+            }, "LoggerQueueMain")
         }
-    }, "LoggerQueue")
+        launch {
+            jedisPool.resource.subscribe(object : JedisPubSub() {
+                override fun onMessage(channel: String, message: String) {
+                    println(Json.decodeFromString<MessageLogDao>(message))
+                    logDb.insertLog(Json.decodeFromString<MessageLogDao>(message))
+                }
+            }, "LoggerQueuePsql")
+        }
+        launch {
+            jedisPool.resource.subscribe(object : JedisPubSub() {
+                override fun onMessage(channel: String, message: String) {
+                    println(Json.decodeFromString<MessageLogDao>(message))
+                    logDb.insertLog(Json.decodeFromString<MessageLogDao>(message))
+                }
+            }, "LoggerQueueTester")
+        }
+    }
 }
